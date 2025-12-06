f571d0ab3e6ef0146d1db5b6aeb7df554e7aa53c
Donovan Blake <donovanjblake@gmail.com>
2025-12-06 08:22:35 -0500

    versionid is an int with copy now

--- src/cache.rs
+++ src/cache.rs
@@ -26,25 +26,27 @@ impl ModFileManager {
     }
 
     /// Construct the path to a cached download file
-    fn cache_path(&self, version_id: &VersionId, filename: &String) -> PathBuf {
+    fn cache_path(&self, version_id: VersionId, filename: &String) -> PathBuf {
+        let version_id = version_id.to_string();
         self.data_dir
-            .join(&version_id.as_str()[0..2])
-            .join(&version_id.as_str()[2..])
+        .join(&version_id[0..2])
+        .join(&version_id[2..])
             .join(filename)
     }
 
     /// Return the location of a cached download file
-    pub fn find_file(&self, version_id: &VersionId, filename: &String) -> Option<PathBuf> {
+    pub fn find_file(&self, version_id: VersionId, filename: &String) -> Option<PathBuf> {
+        let version_id = version_id.to_string();
         let path = self
             .data_dir
-            .join(&version_id.as_str()[0..2])
-            .join(&version_id.as_str()[2..])
+            .join(&version_id[0..2])
+            .join(&version_id[2..])
             .join(filename);
         if !path.is_file() { None } else { Some(path) }
     }
 
     /// Download a file to the data cache directory
-    pub fn download_file(&self, version_id: &VersionId, mod_file: &ModFile) -> Result<PathBuf> {
+    pub fn download_file(&self, version_id: VersionId, mod_file: &ModFile) -> Result<PathBuf> {
         let buffer = mcmod_client::download_file(&mod_file.url)?;
         let path = self.cache_path(version_id, &mod_file.name);
         std::fs::create_dir_all(
@@ -56,7 +58,7 @@ impl ModFileManager {
     }
 
     /// Get a file from the data cache, downloading it if necessary
-    pub fn get_file(&self, version_id: &VersionId, mod_file: &ModFile) -> Result<PathBuf> {
+    pub fn get_file(&self, version_id: VersionId, mod_file: &ModFile) -> Result<PathBuf> {
         if let Some(path) = self.find_file(version_id, &mod_file.name) {
             return Ok(path);
         }
@@ -76,7 +78,7 @@ impl ModFileManager {
 
     pub fn install_file(
         &self,
-        version_id: &VersionId,
+        version_id: VersionId,
         mod_file: &ModFile,
         loader: Option<ModLoader>,
     ) -> Result<()> {
